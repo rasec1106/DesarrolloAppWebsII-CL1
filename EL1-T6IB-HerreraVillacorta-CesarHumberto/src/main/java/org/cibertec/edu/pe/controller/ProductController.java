4ec7eed644fb795fa3a512211357bb022326d9d5
rasec1106 <elch.herrera@gmail.com>
2023-09-27 21:39:09 -0500

    Finish CRUD in Product Controller

--- EL1-T6IB-HerreraVillacorta-CesarHumberto/src/main/java/org/cibertec/edu/pe/controller/ProductController.java
+++ EL1-T6IB-HerreraVillacorta-CesarHumberto/src/main/java/org/cibertec/edu/pe/controller/ProductController.java
@@ -8,8 +8,12 @@ import org.cibertec.edu.pe.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -31,5 +35,30 @@ public class ProductController {
 		Optional<Product> p = service.search(id);
 		m.addAttribute("product", p);
 		return "searchProductById";
+	}
+	
+	@GetMapping("/create")
+	public String create(Model m) {
+		m.addAttribute("product", new Product());
+		return "createNewProduct";
+	}
+	
+	@PostMapping("/save")
+	public String save(@Validated Product p, Model m) {
+		service.save(p);
+		return "redirect:/list";
+	}
+	
+	@GetMapping("/update/{id}")
+	public String update(@PathVariable String id, Model m) {
+		Optional<Product> p = service.search(id);
+		m.addAttribute("product", p);
+		return "updateProduct";
+	}
+	
+	@GetMapping("/delete")
+	public String delete(@PathVariable String id, Model m) {
+		service.delete(id);
+		return "redirect:/list";
 	}
 }
