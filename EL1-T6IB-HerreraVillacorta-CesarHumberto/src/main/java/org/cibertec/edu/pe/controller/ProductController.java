cd756929e2e9cea26f9011ac7521b462be578640
rasec1106 <elch.herrera@gmail.com>
2023-09-27 21:11:31 -0500

    Implement search method

--- EL1-T6IB-HerreraVillacorta-CesarHumberto/src/main/java/org/cibertec/edu/pe/controller/ProductController.java
+++ EL1-T6IB-HerreraVillacorta-CesarHumberto/src/main/java/org/cibertec/edu/pe/controller/ProductController.java
@@ -1,6 +1,7 @@
 package org.cibertec.edu.pe.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.cibertec.edu.pe.interfaceService.IProductService;
 import org.cibertec.edu.pe.model.Product;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,5 +24,12 @@ public class ProductController {
 		List<Product> list = service.list();
 		m.addAttribute("productList", list);
 		return "listProducts";
+	}
+	
+	@GetMapping("/search/{id}")
+	public String search(@PathVariable String id, Model m) {
+		Optional<Product> p = service.search(id);
+		m.addAttribute("product", p);
+		return "searchProductById";
 	}
 }
--- EL1-T6IB-HerreraVillacorta-CesarHumberto/src/main/java/org/cibertec/edu/pe/service/ProductService.java
+++ EL1-T6IB-HerreraVillacorta-CesarHumberto/src/main/java/org/cibertec/edu/pe/service/ProductService.java
@@ -7,7 +7,9 @@ import org.cibertec.edu.pe.interfaceService.IProductService;
 import org.cibertec.edu.pe.interfaces.IProduct;
 import org.cibertec.edu.pe.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProductService implements IProductService {
 	
 	@Autowired
