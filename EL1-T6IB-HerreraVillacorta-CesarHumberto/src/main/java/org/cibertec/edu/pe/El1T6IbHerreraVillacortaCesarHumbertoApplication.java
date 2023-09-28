dfd51978faa6abc69e33456867473f5d57a04716
rasec1106 <elch.herrera@gmail.com>
2023-09-27 19:34:43 -0500

    Create empty Java Maven project

---
+++ EL1-T6IB-HerreraVillacorta-CesarHumberto/src/main/java/org/cibertec/edu/pe/El1T6IbHerreraVillacortaCesarHumbertoApplication.java
@@ -0,0 +1,13 @@
+package org.cibertec.edu.pe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class El1T6IbHerreraVillacortaCesarHumbertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(El1T6IbHerreraVillacortaCesarHumbertoApplication.class, args);
+	}
+
+}
