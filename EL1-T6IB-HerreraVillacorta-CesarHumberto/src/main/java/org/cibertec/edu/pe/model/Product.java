bb6d49a994989f3107f09562dbaca27288dc2f4a
rasec1106 <elch.herrera@gmail.com>
2023-09-27 19:43:38 -0500

    Create Product class

---
+++ EL1-T6IB-HerreraVillacorta-CesarHumberto/src/main/java/org/cibertec/edu/pe/model/Product.java
@@ -0,0 +1,60 @@
+package org.cibertec.edu.pe.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product")
+public class Product {
+	@Id
+	private String id;
+	private String name;
+	private String description;
+	private double price;
+	
+	public Product() {
+		super();
+	}
+
+	public Product(String id, String name, String description, double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+}
